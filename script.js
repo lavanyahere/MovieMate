e36a4b6a4779b3b5aadde8f6ed46377a7562b66f
lavanyahere <157588956+lavanyahere@users.noreply.github.com>
2024-12-19 15:30:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// window.onload = function () {
+//   let username = localStorage.getItem("username");
+//   let content = localStorage.getItem("content");
+
+//   // If a review exists, create a new comment element
+//   if (username && content) {
+//     let div = document.createElement("div");
+//     div.classList.add("comment");
+//     div.innerHTML = `<span class="user-name">${username}</span>
+//                      <p class="comment-text">${content}</p>`;
+//     document.querySelector(".comments").appendChild(div);
+//   }
+// };
